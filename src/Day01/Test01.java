b40fbec0d9b9412294e907147e1c468234e3043c
2205539050 <2205539050@qq.com>
2023-03-06 10:24:42 +0800

    java

---
+++ src/Day01/Test01.java
@@ -0,0 +1,14 @@
+package Day01;
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("byte");
+        System.out.println("short");
+        System.out.println("int");
+        System.out.println("long");
+        System.out.println("char");
+        System.out.println("double");
+        System.out.println("float");
+        System.out.println("boolean");
+    }
+}
